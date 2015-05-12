f3b077fcf9871a8128756e59e8ee0ffda1886caa
Michal Takac <takacmichal92@gmail.com>
2015-05-12 12:48:11 +0200

    removed console.log statements

--- server/braintree-webhook-functions.js
+++ server/braintree-webhook-functions.js
@@ -1,11 +1,11 @@
 /*
-* Stripe Webhook Functions
-* Functions for handling data sent to us from Stripe via webhooks.
+* Braintree Webhook Functions
+* Functions for handling data sent to us from Braintree via webhooks.
 */
 
 btUpdateSubscription = function(request){
-  // Because Stripe doesn't have our Meteor user's ID, we need to do a quick
-  // lookup on our user's collection per the customerId Stripe gives us.
+  // Because Braintree doesn't have our Meteor user's ID, we need to do a quick
+  // lookup on our user's collection per the customerId Braintree gives us.
   var transaction = request.transactions;
   var customerId  = transaction[transaction.length-1].customer.id;
 
@@ -32,19 +32,19 @@ btUpdateSubscription = function(request){
 }
 
 btCreateInvoice = function(request){
-  // Because Stripe doesn't have our Meteor user's ID, we need to do a quick
-  // lookup on our user's collection per the customerId Stripe gives us.
+  // Because Braintree doesn't have our Meteor user's ID, we need to do a quick
+  // lookup on our user's collection per the customerId Braintree gives us.
   var transaction = request.transactions;
   var customerId  = transaction[transaction.length-1].customer.id;
 
   var getUser = Meteor.users.findOne({"customerId": customerId}, {fields: {"_id": 1, "emails.address": 1}});
-  console.log("User: " +getUser);
+
   if (getUser){
-    // Cache the invoice item from Stripe.
+    // Cache the invoice item from Braintree.
     var invoiceItem = request;
     var trial = request.trialPeriod;
 
-    // Make sure that our invoice is greater than $0. We do this because Stripe
+    // Make sure that our invoice doesn't have trial status. We do this because Braintree
     // generates an invoice for our customer's trial (for $0), even though they
     // technically haven't *paid* anything.
     if (trial === false) {
@@ -58,13 +58,12 @@ btCreateInvoice = function(request){
         amount: invoiceItem.nextBillAmount,
         transactionId: Random.hexString(10)
       }
-      console.log("Invoice object:" +Object.keys(invoice));
+
       // Perform our insert. Note: we're doing this here because we'll only ever
       // add invoices via this function. Since we're not sharing it with another
       // operation in the app, we can just isolate it here.
       Invoices.insert(invoice, function(error, response){
         if (error){
-          console.log("Nepodrailo sa vlozit invoice do databazy!");
           console.log(error);
         }
       });
